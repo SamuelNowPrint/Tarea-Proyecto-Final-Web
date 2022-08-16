3e9d4c68dad20df43ab4ff529378d72485c57bfd
SamuelNowPrint <samue@DESKTOP-G1U4RHO>
2022-08-15 23:33:03 -0400

    mi primer commit

---
+++ src/Components/Post/Post.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+export default function Post({ post }) {
+    const { nombre, apellido, comentario } = post;
+    return (
+        <div className="post">
+            <div className="postHeader">
+            <div className="decoration">
+          <span className="material-icons md-60"> account_circle </span>
+        </div>
+            <div className="datos">
+          <p>
+           {nombre} {apellido}
+          </p>
+
+         </div>
+
+            </div>
+            <div className="postContent">
+                <p>{comentario}</p>
+            </div>
+        </div>
+  )
+}
+
+
+
+
+  //   <div className="post">
+    //       <div className="postheader">
+    //       <div className="decoration">
+    //       <span className="material-icons md-60"> account_circle </span>
+    //     </div>
+    //     <div className="datos">
+    //       <p>
+    //         {nombre} {apellido}
+    //       </p>
+        
+    //     </div>
+              
+    //       </div>
+     
+    //     <p>{comentario}</p>
+     
+    //   </div>
